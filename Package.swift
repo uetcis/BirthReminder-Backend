a8220d8cf46d6c46269322303a24d379d792f454
CaptainYukinoshitaHachiman <CaptainYukinoshitaHachiman@protonmail.com>
2017-12-17 10:59:03 +0800

    Update

--- Package.swift
+++ Package.swift
@@ -1,10 +1,30 @@
 import PackageDescription
 
+#if os(Linux)
+    import Glibc
+#else
+    import Darwin
+#endif
+
+var HTTPServer = "https://github.com/PerfectlySoft/Perfect-HTTPServer.git"
+var Notifications = "https://github.com/PerfectlySoft/Perfect-Notifications.git"
+var MySQL = "https://github.com/PerfectlySoft/Perfect-MySQL.git"
+
+if let envHTTPServer = getenv("URL_PERFECT_HTTPSERVER") {
+    HTTPServer = String(cString: envHTTPServer)
+}
+if let envNotifications = getenv("URL_PERFECT_NOTIFICATIONS") {
+    Notifications = String(cString: envNotifications)
+}
+if let envMySQL = getenv("URL_PERFECT_MYSQL") {
+    MySQL = String(cString: envMySQL)
+}
+
 let package = Package(
-	name: "BirthReminderBackend", targets: [],
-	dependencies: [
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 3),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-Notifications.git", majorVersion: 3),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-MySQL.git", majorVersion: 3)
-	]
+    name: "BirthReminderBackend", targets: [],
+    dependencies: [
+        .Package(url: HTTPServer, majorVersion: 3),
+        .Package(url: Notifications, majorVersion: 3),
+        .Package(url: MySQL, majorVersion: 3)
+    ]
 )
--- Sources/BirthReminderBackend/main.swift
+++ Sources/BirthReminderBackend/main.swift
@@ -20,7 +20,7 @@ sslServer.documentRoot = "/webroot"
 sslServer.serverPort = 443 //Port defination
 sslServer.caCert = "/certificates/www.tcwq.tech.ca-bundle"
 sslServer.ssl = (sslCert: "/certificates/www.tcwq.tech.crt", sslKey: "/certificates/www.tcwq.tech.key")
-
+ 
 //API
 var routes = Routes()
 

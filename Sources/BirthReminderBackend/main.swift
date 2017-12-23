3d53ff5340aadbd708a3b3bd1246102054fe8af7
CaptainYukinoshitaHachiman <CaptainYukinoshitaHachiman@protonmail.com>
2017-12-23 12:16:58 +0800

    update

--- Sources/BirthReminderBackend/main.swift
+++ Sources/BirthReminderBackend/main.swift
@@ -20,7 +20,29 @@ sslServer.documentRoot = "/webroot"
 sslServer.serverPort = 443 //Port defination
 sslServer.caCert = "/certificates/www.tcwq.tech.ca-bundle"
 sslServer.ssl = (sslCert: "/certificates/www.tcwq.tech.crt", sslKey: "/certificates/www.tcwq.tech.key")
- 
+
+// Database Configuration
+let host: String
+let user: String
+let password: String
+let database: String
+
+do {
+    let file = File("/.BirthReminder.json")
+    try file.open()
+    let config = try file.readString()
+    let json = try config.jsonDecode() as! [String:String]
+    host = json["host"]!
+    user = json["user"]!
+    password = json["password"]!
+    database = json["database"]!
+    defer {
+        file.close()
+    }
+} catch {
+    fatalError(error.localizedDescription)
+}
+
 //API
 var routes = Routes()
 

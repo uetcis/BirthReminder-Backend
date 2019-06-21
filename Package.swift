be2ab290c77ef5662098e7b32424fb564bbfad5a
CaptainYukinoshitaHachiman <CaptainYukinoshitaHachiman@ProtonMail.com>
2019-06-21 15:38:17 +0800

    Update SPM to 4.0

--- Package.swift
+++ Package.swift
@@ -1,12 +1,26 @@
+// swift-tools-version:4.0
+
 import PackageDescription
 
 let package = Package(
-    name: "BirthReminderBackend", targets: [],
+    name: "BirthReminderBackend",
+    products: [
+        .executable(name: "App", targets: ["App"])
+    ],
     dependencies: [
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-Notifications.git", majorVersion: 3),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", majorVersion: 3),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-MySQL.git", majorVersion: 3),
-        .Package(url: "https://github.com/SvenTiigi/PerfectSlackAPIClient.git", majorVersion: 1),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-RequestLogger.git", majorVersion: 3)
+        .package(url: "https://github.com/PerfectlySoft/Perfect-Notifications.git", from: "3.0.0"),
+        .package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", from: "3.0.0"),
+        .package(url: "https://github.com/PerfectlySoft/Perfect-MySQL.git", from: "3.0.0"),
+        .package(url: "https://github.com/CaptainYukinoshitaHachiman/PerfectSlackAPIClient.git", from: "1.0.0"),
+        .package(url: "https://github.com/PerfectlySoft/Perfect-RequestLogger.git", from: "3.0.0")
+    ],
+    targets: [
+        .target(name: "App",
+                dependencies: [
+                    "Perfect-Notifications",
+                    "Perfect-Logger",
+                    "Perfect-MySQL",
+                    "PerfectSlackAPIClient",
+                    "Perfect-RequestLogger"])
     ]
 )
